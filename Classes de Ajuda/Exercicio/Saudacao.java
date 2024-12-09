9e9cef7f20cacb19df17fb1355c6602c9d272df9
mary0077 <marianagomes201@hotmail.com>
2024-12-09 19:01:30 -0300

    exer 02

---
+++ Classes de Ajuda/Exercicio/Saudacao.java
@@ -0,0 +1,23 @@
+public import java.util.Scanner;
+
+public class Saudacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita o nome do usuário
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        // Solicita a idade do usuário
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        // Exibe a saudação
+        System.out.println("Olá, " + nome + "! Você tem " + idade + " anos.");
+
+        scanner.close();
+    }
+}
+ {
+    
+}
