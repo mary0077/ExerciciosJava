7340753610c1d515558365a67b9ff930012d837b
mary0077 <marianagomes201@hotmail.com>
2025-04-03 20:00:41 -0300

    exercicio contagem regressiva

---
+++ Classes de Ajuda/Exercicio/ContagemRegressiva.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para iniciar a contagem regressiva: ");
+        int numero = scanner.nextInt();
+
+        for (int i = numero; i >= 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Fim da contagem!");
+        scanner.close();
+    }
+}
